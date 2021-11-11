afd54cccd7adec2a07e2a41a3cdc82c796d75c2f
Alina Demeneva <plaguegrim@yandex.ru>
2021-11-11 17:53:26 +0300

    part of homework

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
-fsd
+
+/*
+const a = -3 ;
+
+if (a === 0){
+  console.log("Верно");
+} else{
+  console.log("Неверно");
+}
+*/
+
+/*
+const a = -3;
+
+if (a > 0){
+  console.log("Верно");
+} else{
+  console.log("Неверно");
+}
+*/
+
+/*
+const a = -3;
+
+if (a < 0){
+  console.log("Верно");
+} else{
+  console.log("Неверно");
+}
+*/
+
+/*
+const a = 1;
+
+if (a >= 0){
+  console.log("Верно");
+} else{
+  console.log("Неверно");
+}
+*/
+
+/*
+const a = 1;
+
+if (a <= 0){
+  console.log("Верно");
+} else{
+  console.log("Неверно");
+}
+*/
+
+/*
+const a = 0;
+
+if (a != 0){
+  console.log("Верно");
+} else{
+  console.log("Неверно");
+}
+*/
+
+
+/*
+const a = "тест";
+
+if (a === "test"){
+  console.log("Верно");
+} else{
+  console.log("Неверно");
+}
+*/
+
+/*ЛОГИЧЕСКИЕ ОПЕРАТОРЫ */
+
+/*
+const test = false;
+
+if (test === true){
+  console.log("Верно");
+} else{
+  console.log("Неверно");
+}
+*/
+const test = false;
+
+let result = (test === true) ? console.log("Верно") : console.log("Неверно")
